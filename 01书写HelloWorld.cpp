8dcacb0f69d609b96eace02d11915ecba4a60e5f
moumou036 <100986920+moumou036@users.noreply.github.com>
2022-03-05 14:17:47 +0800

    Hello world

---
+++ 01书写HelloWorld.cpp
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<iostream>
+//using namespace std;
+////描述信息；单行注释
+///*描述信息*/ 
+////多行注释
+//
+//int main()
+//{
+//
+//	cout << "Hello World" << endl;
+//
+//	system("pause");
+//
+//	return 0;
+//}
